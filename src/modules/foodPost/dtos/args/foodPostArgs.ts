f753cff3666f4b92344452b8017fd3eaf529556e
dev-nohyj <goky1129@gmail.com>
2023-08-29 18:36:31 +0900

    Fix/args

--- src/modules/foodPost/dtos/args/foodPostArgs.ts
+++ src/modules/foodPost/dtos/args/foodPostArgs.ts
@@ -2,36 +2,22 @@ import { ApiProperty } from '@nestjs/swagger';
 import { Type } from 'class-transformer';
 import {
     IsArray,
+    IsEnum,
     IsNotEmpty,
     IsNumber,
     IsOptional,
     IsString,
     Length,
+    Min,
     ValidateIf,
     ValidateNested,
 } from 'class-validator';
 import { customErrorLabel } from 'src/asset/labels/error';
 import { CustomError } from 'src/error/custom.error';
+import { FoodPostIdArgs } from './commonArgs';
+import { LikeTypeLabel } from 'src/asset/labels/common';
 
-export class ModifyFoodPostArgs {
-    @ApiProperty({
-        default: 1,
-        description: 'id',
-    })
-    @IsNumber()
-    @IsNotEmpty()
-    @Type(() => Number)
-    foodPostId: number;
-
-    @ApiProperty({
-        default: 'https://www.naverImage.com/image.png',
-        description: '메인 이미지',
-    })
-    @IsString()
-    @IsNotEmpty()
-    @Length(1, 300)
-    mainImageUrl: string;
-
+export class CreateFoodPostArgs {
     @ApiProperty({
         default: '내용',
         description: '내용',
@@ -61,35 +47,71 @@ export class ModifyFoodPostArgs {
     tags: string[] | null;
 
     @ApiProperty({
-        default: [
-            { id: 1, url: 'https://www.naverImage.com/image.png' },
-            { url: 'https://www.naverImage.com/image2.png' },
-        ],
-        description: '이미지 url(최대 6개), nullable',
+        default: [{ url: 'https://www.naverImage.com/image.png' }, { url: 'https://www.naverImage.com/image2.png' }],
+        description: '이미지 url(최대 6개)',
     })
     @IsArray()
     @ValidateNested({ each: true })
-    @Type(() => OtherImage)
-    @ValidateIf((_, value: OtherImage[] | null) => {
+    @Type(() => FoodImages)
+    @ValidateIf((_, value: { url: string }[]) => {
+        if (value === null) {
+            throw new CustomError({ customError: customErrorLabel.INVALID_DATA_TYPE.customError });
+        }
         if (value && value.length === 0) {
             throw new CustomError({ customError: customErrorLabel.INVALID_DATA_TYPE.customError });
         }
         if (value && value.length > 6) {
-            throw new CustomError({ customError: customErrorLabel.RECIPE_POST_MAX_TAG.customError });
+            throw new CustomError({ customError: customErrorLabel.MAX_FOOD_POST_IMAGES.customError });
         }
 
-        return value !== null;
+        return !!value;
     })
-    otherImages: OtherImage[] | null;
+    foodImages: FoodImages[];
 }
-
-class OtherImage {
-    @IsNumber()
-    @IsOptional()
-    @Type(() => Number)
-    id: number | undefined;
-
+class FoodImages {
     @IsString()
     @IsNotEmpty()
     url: string;
+}
+
+export class GetFoodPostsArgs {
+    @ApiProperty({
+        default: 20,
+        description: 'size',
+    })
+    @IsNumber()
+    @IsNotEmpty()
+    @Type(() => Number)
+    size: number;
+
+    @ApiProperty({
+        description: 'cursor (post lastId)',
+        required: false,
+    })
+    @IsOptional()
+    @IsNumber()
+    @Min(1)
+    @IsNotEmpty()
+    @Type(() => Number)
+    cursor: number;
+}
+
+export class LikeFoodPostArgs extends FoodPostIdArgs {
+    @ApiProperty({
+        default: LikeTypeLabel.like,
+        description: 'like type',
+    })
+    @IsEnum(LikeTypeLabel)
+    likeType: LikeTypeLabel;
+}
+
+export class ModifyFoodPostArgs extends CreateFoodPostArgs {
+    @ApiProperty({
+        default: 1,
+        description: 'id',
+    })
+    @IsNumber()
+    @IsNotEmpty()
+    @Type(() => Number)
+    foodPostId: number;
 }
---
+++ src/modules/foodPost/dtos/res/foodPostRes.ts
@@ -0,0 +1,163 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { LikeTypeLabel } from 'src/asset/labels/common';
+
+export class DeleteFoodPostRes {
+    @ApiProperty({
+        default: 1,
+        description: 'id',
+    })
+    foodPostId: number;
+}
+
+export class LikeFoodPostRes {
+    @ApiProperty({
+        default: 1,
+        description: 'foodPostId',
+    })
+    foodPostId: number;
+
+    @ApiProperty({
+        default: LikeTypeLabel.like,
+        description: 'like type',
+    })
+    likeType: LikeTypeLabel;
+}
+
+export class GetFoodPostsRes {
+    @ApiProperty({
+        default: true,
+        description: 'hasMore',
+    })
+    hasMore: boolean;
+
+    @ApiProperty({
+        default: [
+            {
+                id: 1,
+                description: '내용',
+                author: { nickname: 'hi', profileImageUrl: 'https://profileImage.com' },
+                imageUrl: 'https://www.imageUrl',
+                isLike: true,
+                likeCount: 0,
+                createdAt: new Date(),
+                updatedAt: new Date(),
+            },
+            {
+                id: 2,
+                description: '내용',
+                author: { nickname: 'hi', profileImageUrl: 'https://profileImage.com' },
+                imageUrl: 'https://www.imageUrl',
+                isLike: true,
+                likeCount: 0,
+                createdAt: new Date(),
+                updatedAt: new Date(),
+            },
+        ],
+    })
+    foodPostList: {
+        id: number;
+        description: string | null;
+        author: { nickname: string; profileImageUrl: string | null };
+        imageUrl: string;
+        isLike: boolean;
+        likeCount: number;
+        createdAt: Date;
+        updatedAt: Date;
+    }[];
+}
+
+export class GetFoodPostDetailRes {
+    @ApiProperty({
+        default: 1,
+        description: 'id',
+    })
+    id: number;
+    @ApiProperty({
+        default: '내용',
+        description: '내용',
+    })
+    description: string | null;
+    @ApiProperty({
+        default: { id: 'uuid', nickname: 'nickname', profileImageUrl: 'https://profilImageUrl' },
+        description: 'author info',
+    })
+    author: {
+        id: string;
+        nickname: string;
+        profileImageUrl: string | null;
+    };
+    @ApiProperty({
+        default: [
+            { id: 1, url: 'https://foodImageUrl1' },
+            { id: 2, url: 'https://foodImageUrl2' },
+        ],
+        description: 'food Images',
+    })
+    foodImages: { id: number; url: string }[];
+    @ApiProperty({
+        default: true,
+        description: '좋아요 여부',
+    })
+    isLike: boolean;
+    @ApiProperty({
+        default: 0,
+        description: '좋아요 갯수',
+    })
+    likeCount: number;
+
+    @ApiProperty({
+        default: [
+            { id: 1, title: 'hi' },
+            { id: 2, title: 'hello' },
+        ],
+        description: '태그',
+    })
+    tags: { id: number; title: string }[];
+
+    @ApiProperty({
+        description: '생성일',
+    })
+    createdAt: Date;
+    @ApiProperty({
+        description: '수정일',
+    })
+    updatedAt: Date;
+}
+
+export class CreateFoodPostRes {
+    @ApiProperty({
+        default: 1,
+        description: 'id',
+    })
+    id: number;
+
+    @ApiProperty({
+        default: '내용',
+        description: '내용',
+    })
+    description: string | null;
+
+    @ApiProperty({
+        default: { nickname: 'nickname', profileImageUrl: 'https://profilImageUrl' },
+        description: 'author info',
+    })
+    author: {
+        nickname: string;
+        profileImageUrl: string | null;
+    };
+
+    @ApiProperty({
+        default: 'https://foodImage',
+        description: 'foodImage',
+    })
+    imageUrl: string;
+
+    @ApiProperty({
+        description: '생성일',
+    })
+    createdAt: Date;
+    @ApiProperty({
+        description: '수정일',
+    })
+    updatedAt: Date;
+}
